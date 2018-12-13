463c29397737ffa2a78841686aefe3bf5b1c6291
ggulker <ceagentman17@gmail.com>
2018-12-12 20:02:02 -0600

    Add files via upload

---
+++ C Sharp Programs/Program 1/Program 1/Program 1/Form1.cs
@@ -0,0 +1,45 @@
+﻿// Garrett Gulker
+// Due Date: 9/5/18
+// Program 1
+// Create a radio GUI without any accompying code just the visuals
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
