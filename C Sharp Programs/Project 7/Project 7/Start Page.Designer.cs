2dc97a82fade0748eb4f91ec0f790b5c1464c34f
ggulker <gagulker@gmail.com>
2019-09-08 14:01:06 -0500

    Add files via upload

---
+++ C Sharp Programs/Project 7/Project 7/Start Page.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Project_7
+{
+    partial class Background
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.Location = new System.Drawing.Point(12, 104);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(640, 480);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            // 
+            // Background
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 653);
+            this.Controls.Add(this.Canvas);
+            this.Name = "Background";
+            this.Text = "Smiley Face";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Canvas;
+    }
+}
+
---
+++ C Sharp Programs/Project 7/Project 7/Start Page.cs
@@ -0,0 +1,67 @@
+﻿// Garrett Gulker
+// Project 7
+// 11/13/18
+// Create an image from c# graphics 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_7
+{
+    public partial class Background : Form
+    {
+
+        public Background()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            //all brushes used in the program
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+            SolidBrush orangeBrush = new SolidBrush(Color.Orange);
+
+            //background rectangles
+            e.Graphics.FillRectangle(redBrush, 0, 0, 640, 480);
+            Rectangle rect = new Rectangle(10, 10, 460, 370);
+            e.Graphics.FillRectangle(orangeBrush, rect);
+            e.Graphics.FillRectangle(yellowBrush, 20, 20, 440, 350);
+            e.Graphics.FillRectangle(redBrush, 30, 30, 420, 330);
+
+            //string above the face
+            Font font = new Font("Arial", 40);
+            e.Graphics.DrawString("Smile!", font, yellowBrush, 155, 40);
+
+            //left smile
+            e.Graphics.FillEllipse(yellowBrush, 20, 100, 150, 150);
+            e.Graphics.FillEllipse(blackBrush, 40, 120, 30, 30);
+            e.Graphics.FillEllipse(blackBrush, 110, 120, 30, 30);
+            e.Graphics.FillEllipse(blackBrush, 65, 180, 60, 60);
+            e.Graphics.FillRectangle(yellowBrush, 60, 160, 70, 60);
+
+            //right face
+            e.Graphics.FillEllipse(yellowBrush, 300, 100, 150, 150);
+            e.Graphics.FillEllipse(blackBrush, 320, 120, 30, 30);
+            e.Graphics.FillEllipse(blackBrush, 390, 120, 30, 30);
+            e.Graphics.FillEllipse(blackBrush, 345, 180, 60, 60);
+            e.Graphics.FillRectangle(yellowBrush, 340, 160, 70, 60);
+
+            //center face
+            e.Graphics.FillEllipse(yellowBrush, 135, 100, 200, 200);
+            e.Graphics.FillEllipse(blackBrush, 160, 130, 50, 50);
+            e.Graphics.FillEllipse(blackBrush, 260, 130, 50, 50);
+            e.Graphics.FillEllipse(blackBrush, 190, 205, 90, 90);
+            e.Graphics.FillRectangle(yellowBrush, 190, 190, 90, 70);
+
+        }
+    }
+}
